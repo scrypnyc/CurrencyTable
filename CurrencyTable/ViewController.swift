c3648fb60abcb28de7ab8a90994c30c6de063968
Alexander <alexanderlookua@gmail.com>
2019-07-11 19:43:06 +0300

    Initial Commit

---
+++ CurrencyTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyTable
+//
+//  Created by Alexander Skrypnyk on 7/11/19.
+//  Copyright © 2019 Alexander Skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
